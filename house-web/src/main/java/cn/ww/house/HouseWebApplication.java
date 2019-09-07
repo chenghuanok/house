fa2cf31330f0e1f939390d7c707f1a06db1202b2
chenghuan <1832398432@qq.com>
2019-09-07 19:56:58 +0800

    Initial commit

---
+++ house-web/src/main/java/cn/ww/house/HouseWebApplication.java
@@ -0,0 +1,12 @@
+package cn.ww.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseWebApplication.class, args);
+    }
+}
